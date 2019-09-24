a66c3e0b837d87390bbd1856dd9a39aec5fa7462
Kyle Jones <KyleJonesWinsted@gmail.com>
2019-09-23 20:05:40 -0500

    added basic sidebar functionality

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('homepage.html')
 
+@app.route('/browse')
+def browse():
+    return render_template('browse.html')
+
+@app.route('/adjustments')
+def adjustments():
+    return render_template('adjustments.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/search/<search_string>')
+def search(search_string):
+    return render_template('search.html', search_string=search_string)
+
 if __name__ == '__main__':
     app.run(debug=True)
