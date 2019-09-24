929e9554778d77fc2487955d062c386e61ebaec0
Kyle Jones <KyleJonesWinsted@gmail.com>
2019-09-23 20:53:38 -0500

    added basic search functionality

---
+++ static/scripts/base.js
@@ -0,0 +1,16 @@
+const searchBox = document.getElementById('search-box');
+const searchButton = document.getElementById('search-button');
+searchButton.addEventListener("click", search);
+searchBox.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        search()
+    }
+})
+
+function search() {
+    var searchString = searchBox.value;
+    if (searchString == "") { return; }
+    const baseURL = '/search/'
+    // Will probably change later to escape on the server side
+    location.replace(baseURL + encodeURI(searchString)) 
+}
