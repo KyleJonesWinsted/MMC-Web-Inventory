02fa8b3a9954207ba2d3e4ede9cf297f7a23e6f9
Kyle Jones <KyleJonesWinsted@gmail.com>
2019-09-29 20:21:12 -0500

    added page limit to database queries

--- data_controller.py
+++ data_controller.py
@@ -1,24 +1,27 @@
 from data_model import session, Item, Location, Adjustment, AdjustmentLocation, LocationItem, Employee, AdjustmentReason, Category
 import data_model as db
 
+page_limit = 20
+
 # Browse page database access
 def get_item_by_sku(sku: str) -> Item:
     return session.query(Item).filter(Item.sku==sku).one()
 
-def get_items_by_category_id(category_id: int) -> [Item]:
-    return session.query(Item).filter(Item.category_id==category_id).all()
+def get_items_by_category_id(category_id: int, page:int = 0) -> [Item]:
+    return session.query(Item).filter(Item.category_id==category_id).limit(page_limit).offset(page_limit * page).all()
 
-def get_items_by_manufacturer(manufacturer: str) -> [Item]:
-    return session.query(Item).filter(Item.manufacturer==manufacturer).all()
+def get_items_by_manufacturer(manufacturer: str, page:int = 0) -> [Item]:
+    return session.query(Item).filter(Item.manufacturer==manufacturer).limit(page_limit).offset(page_limit * page).all()
 
-def get_items_by_location_id(location_id: int) -> [Item]:
+def get_items_by_location_id(location_id: int, page:int = 0) -> [Item]:
     items = []
-    for row in session.query(LocationItem).filter(LocationItem.location_id==location_id):
+    query = session.query(LocationItem).filter(LocationItem.location_id==location_id).limit(page_limit).offset(page_limit * page)
+    for row in query:
         items.append(row.item)
     return items
 
-def get_all_items() -> [Item]:
-    return session.query(Item).all()
+def get_all_items(page: int = 0) -> [Item]:
+    return session.query(Item).limit(page_limit).offset(page_limit * page).all()
 
 def get_all_manufacturers() -> [str]:
     manufacturers = []
