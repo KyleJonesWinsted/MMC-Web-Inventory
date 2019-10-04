93374c27cc05fc6c8468d68562b1fa8c56c2e4c7
Kyle Jones <KyleJonesWinsted@gmail.com>
2019-10-03 19:55:37 -0500

    create placeholder page and basic browse page

--- app.py
+++ app.py
@@ -1,24 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import view_controllers.browse
 app = Flask(__name__)
 
+#Homepage
 @app.route('/')
 def index():
     return render_template('homepage.html')
 
+#Browse Inventory
 @app.route('/browse')
 def browse():
-    return view_controllers.browse.setupView()
-    
+    return view_controllers.browse.browse_type_view()
 
+@app.route('/browse/category')
+def browse_by_category():
+    return view_controllers.browse.category_select_view()
+
+@app.route('/browse/location')
+def browse_by_location():
+    return view_controllers.browse.location_select_view()
+
+@app.route('/browse/manufacturer')
+def browse_by_manufacturer():
+    return view_controllers.browse.manufacturer_select_view()
+
+@app.route('/browse/all_items')
+def browse_all_items():
+    try:
+        page_number = int(request.args.get('page'))
+    except:
+        page_number = None
+    return view_controllers.browse.all_items_view(page=page_number)
+    
+#Adjustment History
 @app.route('/adjustments')
 def adjustments():
     return render_template('adjustments.html')
 
+#Manage Settings
 @app.route('/settings')
 def settings():
     return render_template('settings.html')
 
+#Search Inventory
 @app.route('/search')
 @app.route('/search/<search_string>')
 def search(search_string=""):
--- view_controllers/browse.py
+++ view_controllers/browse.py
@@ -1,6 +1,17 @@
 import data_controller as db
 from flask import render_template
 
-def setupView():
-    items = db.get_all_items()
+def browse_type_view():
     return render_template('browse.html')
+
+def category_select_view():
+    return render_template('placeholder.html', page_name='category select view')
+
+def location_select_view():
+    return render_template('placeholder.html', page_name='location select view')
+
+def manufacturer_select_view():
+    return render_template('placeholder.html', page_name='manufacturer select view')
+
+def all_items_view(page = 0):
+    return render_template('placeholder.html', page_name='all items view page: {}'.format(page))
