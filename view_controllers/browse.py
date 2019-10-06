302f0f3eff961686a52aef3bc4610cf3ee0a2243
Kyle Jones <KyleJonesWinsted@gmail.com>
2019-10-05 19:43:41 -0500

    created intermediate browse table

--- view_controllers/browse.py
+++ view_controllers/browse.py
@@ -5,7 +5,11 @@ def browse_type_view():
     return render_template('browse.html')
 
 def category_select_view():
-    return render_template('placeholder.html', page_name='category select view')
+    all_categories = db.get_all_categories()
+    rows = []
+    for category in all_categories:
+        rows.append({'name': category.name, 'number_of_items': len(category.items)})
+    return render_template('intermediate_browse_view.html', browse_type='Category', rows = rows)
 
 def location_select_view():
     return render_template('placeholder.html', page_name='location select view')
