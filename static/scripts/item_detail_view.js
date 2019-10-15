b26f12a1d0e99abe35fe44231aca218eb30119f0
Kyle Jones <KyleJonesWinsted@gmail.com>
2019-10-14 20:32:37 -0500

    added new location request to page js

--- static/scripts/item_detail_view.js
+++ static/scripts/item_detail_view.js
@@ -16,19 +16,22 @@ $(function() {
         const addLocationTextbox = document.getElementById('add-location-textbox');
         addLocationTextbox.setCustomValidity('Enter a valid location name. (ex. "301A01A")');
         if (!addLocationTextbox.validity.patternMismatch) {
-            locationName = addLocationTextbox.value.toUpperCase();           
-            locationLabel = document.createElement('label');
-            quantityInput = document.createElement('input');
-            locationLabel.htmlFor = locationName;
-            locationLabel.innerHTML = locationName;
-            quantityInput.type = "number";
-            quantityInput.defaultValue = 0;
-            locationFieldSet = document.getElementById('locations-fieldset');
-            locationFieldSet.appendChild(locationLabel);
-            locationFieldSet.appendChild(quantityInput);
-            locationFieldSet.appendChild(document.createElement('br'));
-
-            addLocationTextbox.value = "";
+            const locationName = addLocationTextbox.value.toLowerCase();
+            $.get('/api/new_location', {location_name: locationName}).done(function (data) {
+                const location = data;
+                const locationLabel = document.createElement('label');
+                const quantityInput = document.createElement('input');
+                locationLabel.htmlFor = location['location_id'];
+                locationLabel.innerHTML = location['location_name'].toUpperCase();
+                quantityInput.type = "number";
+                quantityInput.defaultValue = 0;
+                const locationFieldSet = document.getElementById('locations-fieldset');
+                locationFieldSet.appendChild(locationLabel);
+                locationFieldSet.appendChild(quantityInput);
+                locationFieldSet.appendChild(document.createElement('br'));
+                addLocationTextbox.value = "";
+            })         
+            
         } else {
             addLocationTextbox.reportValidity();
         }
