7effc45ab221601df9bb5fabb04b20daa04b4627
Kyle Jones <KyleJonesWinsted@gmail.com>
2019-12-05 17:33:00 -0600

    replaced URLSearchParam

--- static/scripts/basic-table-view.js
+++ static/scripts/basic-table-view.js
@@ -9,9 +9,12 @@ $(function() {
         if (event.keyCode == '13') {
             if ($('#page-number')[0].checkValidity()) {
                 var pageNumber = $('#page-number').val();
+                /*
                 parameters.set('page', pageNumber - 1);
                 currentURL.search = parameters.toString();
-                window.location.assign(currentURL.toString()); 
+                window.location.assign(currentURL.toString());
+                */
+                window.location.search = jQuery.query.set("page", pageNumber - 1);
             } else {
                 $('#page-number')[0].reportValidity();
             }         
@@ -19,14 +22,20 @@ $(function() {
     });
 
     $('#next-page-button').click(function() {
+        /*
         parameters.set('page', currentPageNumber + 1);
         currentURL.search = parameters.toString();
         window.location.assign(currentURL.toString());
+        */        
+        window.location.search = jQuery.query.set("page", currentPageNumber + 1);
     });
 
     $('#prev-page-button').click(function() {
+        /*
         parameters.set('page', currentPageNumber - 1);
         currentURL.search = parameters.toString();
         window.location.assign(currentURL.toString());
+        */
+       window.location.search = jQuery.query.set("page", currentPageNumber - 1);
     });
 });
