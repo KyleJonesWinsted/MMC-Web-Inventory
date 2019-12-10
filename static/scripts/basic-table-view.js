5581b1a79d3162f7249848fbb10cbad718946119
Kyle Jones <KyleJonesWinsted@gmail.com>
2019-12-09 20:40:17 -0600

    removed URLSearchParam for custom implementation

--- static/scripts/basic-table-view.js
+++ static/scripts/basic-table-view.js
@@ -1,20 +1,36 @@
 $(function() {
-
-    var currentURL = new URL(window.location.href);     
-    var queries = currentURL.search;
-    var parameters = new URLSearchParams(queries);
-
     //Page navigation
+
+    function createUrlWithPage(page_number) {
+        //Splits parameters from url, creates dict of parameters, modifies 'page' in dict and reattaches to url
+        var url = window.location.href.toString()
+        var splitURL = url.split("?")    
+        var modifiedURL = splitURL[0] + '?'
+        var parameters = {}
+        if (splitURL.length == 2) {
+            var param_strings = splitURL[1].split("&")
+            for (param_string of param_strings) {
+                if (param_string != "") {                    
+                    const keyRegex = new RegExp('(.+)\=')
+                    const valueRegex = new RegExp('\=(.+)')
+                    var key = param_string.match(keyRegex)[1]
+                    var value = param_string.match(valueRegex)[1]
+                    parameters[key] = value
+                }
+            }
+        }
+        parameters['page'] = page_number
+        for (key in parameters) {
+            modifiedURL += key + '=' + parameters[key] + '&'
+        }        
+        return modifiedURL
+    }
+
     $('#page-number').keyup(function(event) {        
         if (event.keyCode == '13') {
             if ($('#page-number')[0].checkValidity()) {
                 var pageNumber = $('#page-number').val();
-                /*
-                parameters.set('page', pageNumber - 1);
-                currentURL.search = parameters.toString();
-                window.location.assign(currentURL.toString());
-                */
-                window.location.search = jQuery.query.set("page", pageNumber - 1);
+                window.location.assign(createUrlWithPage(pageNumber - 1))
             } else {
                 $('#page-number')[0].reportValidity();
             }         
@@ -22,20 +38,10 @@ $(function() {
     });
 
     $('#next-page-button').click(function() {
-        /*
-        parameters.set('page', currentPageNumber + 1);
-        currentURL.search = parameters.toString();
-        window.location.assign(currentURL.toString());
-        */        
-        window.location.search = jQuery.query.set("page", currentPageNumber + 1);
+        window.location.assign(createUrlWithPage(currentPageNumber + 1))      
     });
 
     $('#prev-page-button').click(function() {
-        /*
-        parameters.set('page', currentPageNumber - 1);
-        currentURL.search = parameters.toString();
-        window.location.assign(currentURL.toString());
-        */
-       window.location.search = jQuery.query.set("page", currentPageNumber - 1);
+        window.location.assign(createUrlWithPage(currentPageNumber - 1))
     });
 });
